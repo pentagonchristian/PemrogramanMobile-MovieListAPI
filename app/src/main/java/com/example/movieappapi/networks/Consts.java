6042a9b8a0e9b688c478214930afb940bb60c76e
Pentagon11 <pentagonbatlayar@gmail.com>
2021-05-29 11:08:54 +0700

    MovieList

---
+++ app/src/main/java/com/example/movieappapi/networks/Consts.java
@@ -0,0 +1,12 @@
+package com.example.movieappapi.networks;
+
+public class Consts {
+
+    public static final String BASEURL = "https://api.themoviedb.org/3/";
+    public static final String APIKEY = "a9fb66b9a6cc01e5d8d310073df09473";
+    public static final String LANGUAGE = "en-US";
+    public static final String PAGE = "1";
+    public static final String IMAGEBASEURL = "https://image.tmdb.org/t/p/w500/";
+    public static final String APIERROR = "DEBUG:API ERROR";
+
+}
